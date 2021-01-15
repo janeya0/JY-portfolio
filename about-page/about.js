381e302d01206169e9306dac5a1bddc95a1c72b4
janeya0 <jane.yao321@gmail.com>
2021-01-15 14:32:47 -0500

    About-page

---
+++ about-page/about.js
@@ -0,0 +1,81 @@
+var scroll = window.requestAnimationFrame ||
+             // IE Fallback
+             function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.show-on-scroll');
+
+function loop()  {
+  elementsToShow.forEach(function (element) {
+    if (isElementInViewport(element)) {
+      element.classList.add('is-visible');
+    } else {
+      element.classList.remove('is-visible');
+    }
+  });
+
+  scroll(loop);
+  }
+
+loop();
+
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+
+
+//Scroll to top button
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+//Contact page slide inspect
+var w = $(window).width();
+var h = $(window).height();
+var slides = $('.Slides > div');
+$('.SlideContainer').css({ height: (h-0) + 'px' });
+$('.Slides').css({ width: slides.length + '00%' });
+slides.css({ width: w + 'px' });
+
+var pos = 0;
+
+$('.Left').click(function(){
+  pos--;
+  $('.Slides').animate({ left: (pos * w) + 'px' });
+});
+$('.Right').click(function(){
+  pos++;
+  $('.Slides').animate({ left: (pos * w) + 'px' });
+});
